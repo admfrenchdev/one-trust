b73f27b71e00e4e611a3560a648daf3875e8d49b
admfrenchdev <olivier.auboin@adm.com>
2024-01-10 14:28:21 +0100

    Try to add code from picture

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,10 +7,33 @@ import {
 } from "@ionic/react";
 import ExploreContainer from "../components/ExploreContainer";
 import "./Home.css";
+import { useState } from "react";
 
 declare var OneTrust: any;
+OneTrust.startSDK(
+  "cdn.cookielaw.org",
+  " 5dbf6b9-4546-4651-ac8e-7aa1424bab16-test",
+  "en",
+  null,
+  (downloadStatus: boolean) => {
+    OneTrust.shouldShowBanner((result: boolean) => {
+      if (result && downloadStatus) {
+        OneTrust.showBannerUI();
+      }
+    });
+    OneTrust.getConsentStatusFactory("C0004", (consentStatus: number) => {
+      console.log("The quiered consent status for C0004 is " + consentStatus);
+    });
+  }
+);
 
 const Home: React.FC = () => {
+  const [category4, update] = useState(-1);
+  const updateC4Status = (listenerPayload: any) => {
+    update(listenerPayload.consentStatus);
+  };
+  document.addEventListener("C0004", updateC4Status, false);
+
   console.log("ONETRUST", JSON.stringify(window?.OneTrust ?? {}));
   console.log("ONETRUST", JSON.stringify(OneTrust ?? {}));
   return (
@@ -26,7 +49,7 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <ExploreContainer c4Status={category4} />
       </IonContent>
     </IonPage>
   );
